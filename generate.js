fcf62ba84e44907ac2c0af0a8beeac73e43d0547
Akshay Chintalapati <64036106+akshayvkt@users.noreply.github.com>
2023-07-06 23:56:07 -0700

    skeleton code added

---
+++ generate.js
@@ -0,0 +1,15 @@
+import {configuration, OpenAIApi} from 'openai'
+import {process} from './env'
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const completion = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [{'role':'system','content':'You are a helpful teacher guiding students patiently and cheerfully.'},
+{'role':'user','content':'Hello, who is this?'}]
+})
+console.log(completion.data.choices[0].message);
